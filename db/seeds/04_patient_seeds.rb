72feec44d325befea0cb039b590fbdd2c25df168
Eugenio Gomez <eugesma@gmail.com>
2020-11-19 11:14:59 -0300

    add occupations to seed

--- db/seeds/04_patient_seeds.rb
+++ db/seeds/04_patient_seeds.rb
@@ -1,1 +1,12 @@
 PatientType.create(name: "Ambulatorio")
+
+Occupation.create(name: "Hospital")
+Occupation.create(name: "Policía")
+Occupation.create(name: "Salud (privado)")
+Occupation.create(name: "Bomberos")
+Occupation.create(name: "Defensa Civil")
+Occupation.create(name: "Policía Federal")
+Occupation.create(name: "Prefectura")
+Occupation.create(name: "Ejército")
+Occupation.create(name: "Otro")
+
