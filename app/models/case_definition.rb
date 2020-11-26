b35a5a860be462850d4c4637fe596c401bad1211
Eugenio Gomez <eugesma@gmail.com>
2020-11-26 14:09:03 -0300

    feat: add contacts to epidemic sheet show. validate case definition

--- app/models/case_definition.rb
+++ app/models/case_definition.rb
@@ -2,5 +2,5 @@ class CaseDefinition < ApplicationRecord
   enum case_type: { sospechoso: 0, pendiente: 1, positivo: 2, negativo: 3 }
   belongs_to :diagnostic_method, optional: true
 
-  validates_presence_of :diagnostic_method, if: Proc.new { |case_d| case_d.positivo? || case_d.negativo? }
+  validates_presence_of :diagnostic_method_id, unless: Proc.new { |case_d| case_d.sospechoso? }
 end
