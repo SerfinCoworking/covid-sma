44e1badcb1c5849f5966ca30364533431d80a3c8
Eugenio Gomez <eugesma@gmail.com>
2021-01-04 15:47:41 -0300

    feat: migrate count case per day

---
+++ db/migrate/20210104172758_count_case_per_day_since_monday_twenty_one.rb
@@ -0,0 +1,20 @@
+class CountCasePerDaySinceMondayTwentyOne < ActiveRecord::Migration[5.2]
+  def change
+    CaseCountPerDay.destroy_all
+    i = 0
+    notification_date = Date.new(2020, 12, 21)
+    while i < 14      
+      CaseStatus.find_each do |status|
+        today_epidemic_sheets = status.epidemic_sheets.since_date(notification_date.beginning_of_day).to_date(notification_date.end_of_day)
+        
+        CaseCountPerDay.create(
+          case_status_id: status.id,
+          count: today_epidemic_sheets.count,
+          created_at: notification_date
+        )
+      end
+      notification_date = notification_date + 1.days
+      i += 1
+    end
+  end
+end
