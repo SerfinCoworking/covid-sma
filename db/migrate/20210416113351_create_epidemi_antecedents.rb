7b48363b961ac4312386e2105b90b386c1f51f58
paul <paul.18.sma@gmail.com>
2021-04-20 10:26:16 -0300

    removes vaccines options from antecents

--- db/migrate/20210416113351_create_epidemi_antecedents.rb
+++ db/migrate/20210416113351_create_epidemi_antecedents.rb
@@ -7,8 +7,6 @@ class CreateEpidemiAntecedents < ActiveRecord::Migration[5.2]
 
     EpidemiAntecedent.create(name: "Contacto estrecho de caso confirmado en últimos 14 días")
     EpidemiAntecedent.create(name: "Viaje al exterior en últimos 14 días")
-    EpidemiAntecedent.create(name: "Vacunado covid 1° dosis")
-    EpidemiAntecedent.create(name: "Vacunado covid 2° (agregar fecha de vacunación)")
     EpidemiAntecedent.create(name: "Burbuja escolar")
     EpidemiAntecedent.create(name: "Pertenece a comunidad cerrada")
     EpidemiAntecedent.create(name: "Internación en los últimos 14 días")
