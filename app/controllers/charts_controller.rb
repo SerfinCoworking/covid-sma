e62300c34de22d4073e201354079087f2da1e810
Eugenio Gomez <eugesma@gmail.com>
2021-06-08 09:47:44 -0300

    fix: replicate new scope date

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -2,7 +2,7 @@ class ChartsController < ApplicationController
   def by_month_epidemic_sheets
     render json: EpidemicSheet
       .by_city(current_user.establishment_city)
-      .since_date(Date.today.beginning_of_year)
+      .since_date(Date.today.strftime("%d/%m/%y"))
       .group_by_month_of_year("epidemic_sheets.created_at")
       .count.map{ |k, v| [I18n.t("date.month_names")[k], v]}
   end
