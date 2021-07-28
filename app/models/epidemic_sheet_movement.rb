847a1f40b9c1f9a420a988f374cbdd193023d8bd
Eugenio Gomez <eugesma@gmail.com>
2021-07-28 08:35:23 -0300

    fix: nil user on movement

--- app/models/epidemic_sheet_movement.rb
+++ app/models/epidemic_sheet_movement.rb
@@ -1,5 +1,9 @@
 class EpidemicSheetMovement < ApplicationRecord
+  # Relationships
   belongs_to :epidemic_sheet
   belongs_to :user
   belongs_to :sector
+
+  # Delegations
+  delegate :full_name, to: :user, prefix: true, allow_nil: true
 end
