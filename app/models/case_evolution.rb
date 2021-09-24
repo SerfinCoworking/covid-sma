86524f2acc2ca7e08b18d45a9dc6c1d4641fde13
Eugenio Gomez <eugesma@gmail.com>
2021-09-24 10:18:29 -0300

    feat: back to without SISA when evolution to reinfected patient

--- app/models/case_evolution.rb
+++ app/models/case_evolution.rb
@@ -1,4 +1,6 @@
 class CaseEvolution < ApplicationRecord
+  
+  # Relationships
   belongs_to :case_status
   belongs_to :diagnostic_method
   belongs_to :epidemic_sheet
@@ -6,8 +8,20 @@ class CaseEvolution < ApplicationRecord
   belongs_to :special_device
   has_one :establishment, through: :epidemic_sheet
 
+  # Validations
   validates_presence_of :case_status, :epidemic_sheet, :patient      
   validates_presence_of :diagnostic_method_id, :special_device, if: Proc.new { |case_e| case_e.case_status.needs_diagnostic? }
 
+  # Delegations
   delegate :name, :badge, to: :case_status, prefix: true
+
+  # Callbacks
+  after_create :update_sisa
+
+  private
+
+  def update_sisa
+    epidemic_sheet.is_in_sisa = false if case_status_name == 'Positivo (reinfección)'
+    epidemic_sheet.save!
+  end
 end
