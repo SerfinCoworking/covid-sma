f819338cf3d66ea897c2a24c954e1e24972656a8
paul <paul.18.sma@gmail.com>
2022-01-20 08:07:25 -0300

    changes to 12:30 AM schedule time

--- config/schedule.rb
+++ config/schedule.rb
@@ -29,6 +29,6 @@ every 1.day, at: '11:30 pm' do
   rake "batch:save_case_count_per_days"
 end
 
-every 1.day, at: '11:30 pm' do
+every 1.day, at: '12:30 am' do
   rake "batch:update_restored_positive_cases"
 end
